d307a57fa24241e3039d94608c550843e59c08b8
Axda <axda.web@gmail.com>
2023-04-07 09:01:03 +0200

    Theme: Extends tailwindcss theme + Apply global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#151515',
+        secondary: '#4EE1A0',
+        tertiary: '#242424',
+        silver: '#d9d9d9'
+      },
+      fontSize: {
+        base: '1.125rem',
+        'heading-m': '1.5rem',
+        'heading-l': '3rem',
+        'heading-m': '5.5rem',
+      },
+      fontFamily: {
+        sans: ['var(--font-space-grotesk)']
+      }
+    },
   },
   plugins: [],
 }
