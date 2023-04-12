04f33370ef37a5060669a7f359f2abbee16b0446
Alyx DARENNE <axda.web@gmail.com>
2023-04-12 07:18:30 +0200

    Update: Add Nav component

---
+++ app/components/nav/Nav.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <header className="py-5 sm:py-10 text-heading-m sm:flex sm:flex-row sm:align-middle justify-between space-y-5 sm:space-y-0 flex flex-col items-center">
+      <div className="text-white text-2xl sm:text-3xl">
+        <Link href="/">alyxdarenne</Link>
+      </div>
+      <div className="flex space-x-8">
+        <a href="https://github.com/Axda-Web" target="_blank">
+          <div className="w-5 sm:w-6 h-5 sm:h-6">
+            <Image
+              src="/icons/socials/icon-github.svg"
+              alt="GitHub Logo"
+              width={24}
+              height={24}
+              priority
+            />
+          </div>
+        </a>
+        <a
+          href="https://www.frontendmentor.io/profile/Axda-Web"
+          target="_blank"
+        >
+          <div className="w-5 sm:w-6 h-5 sm:h-6">
+            <Image
+              src="/icons/socials/icon-frontend-mentor.svg"
+              alt="Frontend Mentor Logo"
+              width={24}
+              height={24}
+              priority
+            />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/alyx-darenne" target="_blank">
+          <div className="w-5 sm:w-6 h-5 sm:h-6">
+            <Image
+              src="/icons/socials/icon-linkedin.svg"
+              alt="LinkedIn Logo"
+              width={24}
+              height={24}
+              priority
+            />
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+export default Nav;
