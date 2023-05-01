4dc94b69d88ca4a89403436d65add41bbce82af1
Alyx DARENNE <axda.web@gmail.com>
2023-05-01 17:33:15 +0200

    Contact: Update components

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,11 @@
-type Skill = {
-    id: number;
-    tech: string;
-    exp: number
-}
+import { z } from "zod"
+
+export const skillSchema = z.object({
+    id: z.number(),
+    tech: z.string(),
+    exp: z.number()
+})
+
+type Skill = z.infer<typeof skillSchema>
 
 export type Skills = Skill[]
